4440352315839a479246be61f5d30e40c007e3ef
fhliang <fhliang@leapstack.cn>
2020-06-28 12:55:57 +0800

    20200628 SpringUtil添加组件注解

--- diyi-common/src/main/java/com/lgyun/common/tool/SpringUtil.java
+++ diyi-common/src/main/java/com/lgyun/common/tool/SpringUtil.java
@@ -5,6 +5,7 @@ import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.ApplicationEvent;
+import org.springframework.stereotype.Component;
 
 /**
  * spring 工具类
@@ -13,6 +14,7 @@ import org.springframework.context.ApplicationEvent;
  * @since 2020/6/5 19:08
  */
 @Slf4j
+@Component
 public class SpringUtil implements ApplicationContextAware {
 
     private static ApplicationContext context;
