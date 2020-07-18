84e8ce190486431f78cb7d1390829ab71bc76613
fhliang <fhliang@leapstack.cn>
2020-07-18 21:01:07 +0800

    20200718 mapper bug fix 2

--- diyi-service-api/diyi-user-api/src/main/java/com/lgyun/system/user/entity/OnlineSignPicEntity.java
+++ diyi-service-api/diyi-user-api/src/main/java/com/lgyun/system/user/entity/OnlineSignPicEntity.java
@@ -13,7 +13,7 @@ import lombok.NoArgsConstructor;
 import java.util.Date;
 
 /**
- *  Entity
+ * Entity
  *
  * @author jun
  * @since 2020-07-18 15:59:14
@@ -27,33 +27,33 @@ public class OnlineSignPicEntity extends BaseEntity {
     /**
      * 唯一性控制
      */
-        @ApiModelProperty(value = "主键")
+    @ApiModelProperty(value = "主键")
     @TableId(type = IdType.ASSIGN_ID)
     @JsonSerialize(using = ToStringSerializer.class)
-        private Long id;
+    private Long id;
 
     /**
      * 对象身份1、创客本人2、商户人员3、服务商人员4、相关局人员5、渠道商人员6、合伙人本人
      */
-        private Integer objectType;
+    private Integer objectType;
 
     /**
      * 对象身份ID
      */
-        private Long objectId;
+    private Long objectId;
 
     /**
      * 相关人员ID 创客/合伙人，就取创客/合伙人ID，其余取相关工作人员ID
      */
-        private Long workerSex;
+    private Long workerSex;
 
     /**
      * 签字笔迹URL
      */
-        private String signPic;
+    private String signPic;
 
     /**
      * 签署日期
      */
-        private Date signDatetime;
-    }
+    private Date signDatetime;
+}
