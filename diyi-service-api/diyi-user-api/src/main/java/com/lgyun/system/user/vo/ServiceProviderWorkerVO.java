12ad56baa9d91c7b12713a6e5dc181d3feeb40f0
fhliang <fhliang@leapstack.cn>
2020-09-19 17:08:22 +0800

    20200919 添加 服务商主子账号操作、权限接口

--- diyi-service-api/diyi-user-api/src/main/java/com/lgyun/system/user/vo/ServiceProviderWorkerVO.java
+++ diyi-service-api/diyi-user-api/src/main/java/com/lgyun/system/user/vo/ServiceProviderWorkerVO.java
@@ -2,8 +2,11 @@ package com.lgyun.system.user.vo;
 
 import com.lgyun.system.user.entity.ServiceProviderWorkerEntity;
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+
+import java.util.List;
 
 /**
  * 视图实体类
@@ -17,4 +20,21 @@ import lombok.EqualsAndHashCode;
 public class ServiceProviderWorkerVO extends ServiceProviderWorkerEntity {
     private static final long serialVersionUID = 1L;
 
+    private String accountStateValue;
+
+    private String accountStateDesc;
+
+    private String workerSexValue;
+
+    private String workerSexDesc;
+
+    private String positionNameValue;
+
+    private String positionNameDesc;
+
+    @ApiModelProperty(value = "菜单集合,勾选菜单title集合")
+    private List<String> menuNameList;
+
+    @ApiModelProperty(value = "菜单集合,勾选菜单Id集合")
+    private List<String> menuIds;
 }
