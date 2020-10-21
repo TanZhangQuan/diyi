f8afddccc7f7aab33b362ff36e8eb8e0370929ad
yixi <yixi991010@163.com>
2020-10-21 20:21:13 +0800

    税局管理（完成）

--- diyi-service/diyi-user/src/main/java/com/lgyun/system/user/controller/admin/TaxBureauController.java
+++ diyi-service/diyi-user/src/main/java/com/lgyun/system/user/controller/admin/TaxBureauController.java
@@ -1,6 +1,7 @@
 package com.lgyun.system.user.controller.admin;
 
 import com.lgyun.common.api.R;
+import com.lgyun.common.enumeration.BureauServiceProviderStatus;
 import com.lgyun.common.enumeration.BureauType;
 import com.lgyun.common.secure.BladeUser;
 import com.lgyun.core.mp.support.Condition;
@@ -39,7 +40,7 @@ public class TaxBureauController {
 
     @PostMapping("/query-tax-bureau")
     @ApiOperation(value = "查询税局", notes = "查询税局")
-    public R getTaxBureau(@RequestBody QueryRelBureauListDTO queryRelBureauListDTO, Query query, BladeUser bladeUser){
+    public R getTaxBureau(@RequestBody QueryRelBureauListDTO queryRelBureauListDTO, Query query, BladeUser bladeUser) {
         //查询当前管理员
         R<AdminEntity> result = adminService.currentAdmin(bladeUser);
         if (!(result.isSuccess())) {
@@ -51,7 +52,7 @@ public class TaxBureauController {
 
     @PostMapping("/add-tax-bureau")
     @ApiOperation(value = "添加税局", notes = "添加税局")
-    public R addTaxBureau(@Valid @RequestBody AddRelBureauDTO addRelBureauDto, BladeUser bladeUser){
+    public R addTaxBureau(@Valid @RequestBody AddRelBureauDTO addRelBureauDto, BladeUser bladeUser) {
         //查询当前管理员
         R<AdminEntity> result = adminService.currentAdmin(bladeUser);
         if (!(result.isSuccess())) {
@@ -62,7 +63,7 @@ public class TaxBureauController {
 
     @GetMapping("/query-tax-bureau-info")
     @ApiOperation(value = "查询税局信息", notes = "查询税局信息")
-    public R queryTaxBureauInfo(@ApiParam("税局Id") @NotNull(message = "税局Id不能为空！") @RequestParam(required = false) Long bureauId, BladeUser bladeUser){
+    public R queryTaxBureauInfo(@ApiParam("税局Id") @NotNull(message = "税局Id不能为空！") @RequestParam(required = false) Long bureauId, BladeUser bladeUser) {
         //查询当前管理
         R<AdminEntity> result = adminService.currentAdmin(bladeUser);
         if (!(result.isSuccess())) {
@@ -73,7 +74,7 @@ public class TaxBureauController {
 
     @PostMapping("/update-tax-bureau")
     @ApiOperation(value = "编辑税局", notes = "编辑税局")
-    public R updateTaxBureau(@Valid @RequestBody UpdateRelBureauDTO updateRelBureauDTO, BladeUser bladeUser){
+    public R updateTaxBureau(@Valid @RequestBody UpdateRelBureauDTO updateRelBureauDTO, BladeUser bladeUser) {
         //查询当前管理员
         R<AdminEntity> result = adminService.currentAdmin(bladeUser);
         if (!(result.isSuccess())) {
@@ -84,7 +85,7 @@ public class TaxBureauController {
 
     @GetMapping("/query-tax-bureau-notice")
     @ApiOperation(value = "查询税局信息", notes = "查询税局信息")
-    public R queryTaxBureauNotice(@ApiParam("税局Id") @NotNull(message = "税局Id不能为空！") @RequestParam(required = false) Long bureauId, Query query, BladeUser bladeUser){
+    public R queryTaxBureauNotice(@ApiParam("税局Id") @NotNull(message = "税局Id不能为空！") @RequestParam(required = false) Long bureauId, Query query, BladeUser bladeUser) {
         //查询当前管理
         R<AdminEntity> result = adminService.currentAdmin(bladeUser);
         if (!(result.isSuccess())) {
@@ -95,7 +96,7 @@ public class TaxBureauController {
 
     @GetMapping("/query-tax-bureau-service-provider")
     @ApiOperation(value = "查询可以匹配的服务商", notes = "查询可以匹配的服务商")
-    public R queryTaxBureauServiceProvider(@ApiParam("服务商名称") @RequestParam(required = false) String serviceProviderName, Query query, BladeUser bladeUser){
+    public R queryTaxBureauServiceProvider(@ApiParam("服务商名称") @RequestParam(required = false) String serviceProviderName, Query query, BladeUser bladeUser) {
         //查询当前管理
         R<AdminEntity> result = adminService.currentAdmin(bladeUser);
         if (!(result.isSuccess())) {
@@ -105,16 +106,36 @@ public class TaxBureauController {
     }
 
     @PostMapping("/add-tax-bureau-service-provider")
-    @ApiOperation(value = "给税局添加匹配服务商", notes = "给税局添加匹配服务商")
-    public R addTaxBureauServiceProvider(@ApiParam("服务商ID字符集，每个ID直接用英文逗号隔开") @NotBlank(message = "请选择匹配的服务商！") @RequestParam(required = false) String serviceProviderIds,
-                                         @ApiParam("税局ID") @NotNull(message = "税局ID不能为空！！") @RequestParam(required = false) Long bureauId, BladeUser bladeUser){
+    @ApiOperation(value = "开启或关闭匹配服务商", notes = "开启或关闭匹配服务商")
+    public R addRelBureauServiceProvider(@ApiParam("税局ID不能为空") @NotNull(message = "税局ID不能为空") @RequestParam(required = false) Long bureauId,
+                                         @ApiParam("服务商ID字符集，ID直接用逗号隔开") @NotBlank(message = "匹配服务商不能为空！") @RequestParam(required = false) String serviceProviderIds, BladeUser bladeUser) {
         //查询当前管理
         R<AdminEntity> result = adminService.currentAdmin(bladeUser);
         if (!(result.isSuccess())) {
             return result;
         }
-        return bureauServiceProviderService.addRelBureauServiceProvider(serviceProviderIds,bureauId);
+        return bureauServiceProviderService.addRelBureauServiceProvider(serviceProviderIds, bureauId);
     }
 
+    @PostMapping("/update-tax-bureau-service-provider")
+    @ApiOperation(value = "开启或关闭匹配服务商", notes = "开启或关闭匹配服务商")
+    public R updateTaxBureauServiceProvider(@ApiParam("匹配的ID") @NotNull(message = "匹配的ID不能为空") @RequestParam(required = false) Long bureauServiceProviderId, BureauServiceProviderStatus bureauServiceProviderStatus, BladeUser bladeUser) {
+        //查询当前管理
+        R<AdminEntity> result = adminService.currentAdmin(bladeUser);
+        if (!(result.isSuccess())) {
+            return result;
+        }
+        return bureauServiceProviderService.updateTaxBureauServiceProvider(bureauServiceProviderId, bureauServiceProviderStatus);
+    }
 
+    @PostMapping("/delete-tax-bureau-service-provider")
+    @ApiOperation(value = "撤销服务商", notes = "撤销服务商")
+    public R deleteTaxBureauServiceProvider(@ApiParam("匹配的ID") @NotNull(message = "匹配的ID不能为空") @RequestParam Long bureauServiceProviderId, BladeUser bladeUser) {
+        //查询当前管理
+        R<AdminEntity> result = adminService.currentAdmin(bladeUser);
+        if (!(result.isSuccess())) {
+            return result;
+        }
+        return bureauServiceProviderService.deleteTaxBureauServiceProvider(bureauServiceProviderId);
+    }
 }
