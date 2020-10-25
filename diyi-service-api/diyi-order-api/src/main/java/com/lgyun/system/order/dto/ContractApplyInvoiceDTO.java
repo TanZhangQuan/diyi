d1d33a4cb991522105d9d62aaff518ce71cb3fc6
i1341 <i1341947083@163.com>
2020-10-25 18:12:14 +0800

    修改

--- diyi-service-api/diyi-order-api/src/main/java/com/lgyun/system/order/dto/ContractApplyInvoiceDTO.java
+++ diyi-service-api/diyi-order-api/src/main/java/com/lgyun/system/order/dto/ContractApplyInvoiceDTO.java
@@ -2,11 +2,9 @@ package com.lgyun.system.order.dto;
 
 import lombok.Data;
 
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
-import java.math.BigDecimal;
 
 /**
  * @author tzq
@@ -18,18 +16,17 @@ public class ContractApplyInvoiceDTO implements Serializable {
     private static final long serialVersionUID = 1L;
 
     /**
-     * 支付清单id
+     * 服务商id
      */
-    @NotNull(message = "支付清单id不能为空")
-    private Long payEnterpriseId;
+    @NotNull(message = "服务商id不能为空")
+    private Long serviceProviderId;
 
     /**
-     * 开票总金额
+     * 支付清单id
      */
-    @NotNull(message = "请输入开票总金额")
-    @Min(value = 0, message = "开票总金额不能小于0")
-    @Max(value = 999999999, message = "开票总金额不能大于999999999")
-    private BigDecimal voiceTotalAmount;
+    @NotBlank(message = "支付清单id不能为空")
+    private String payEnterpriseIds;
+
 
     /**
      * 开票类目Id
@@ -41,4 +38,5 @@ public class ContractApplyInvoiceDTO implements Serializable {
      * 说明
      */
     private String applicationDesc;
+
 }
--- diyi-service/diyi-order/src/main/java/com/lgyun/system/order/service/impl/InvoiceApplicationServiceImpl.java
+++ diyi-service/diyi-order/src/main/java/com/lgyun/system/order/service/impl/InvoiceApplicationServiceImpl.java
@@ -3,7 +3,9 @@ package com.lgyun.system.order.service.impl;
 import com.lgyun.common.api.R;
 import com.lgyun.core.mp.base.BaseServiceImpl;
 import com.lgyun.system.order.dto.ContractApplyInvoiceDTO;
-import com.lgyun.system.order.entity.*;
+import com.lgyun.system.order.entity.InvoiceApplicationEntity;
+import com.lgyun.system.order.entity.InvoiceApplicationPayListEntity;
+import com.lgyun.system.order.entity.PayEnterpriseEntity;
 import com.lgyun.system.order.mapper.InvoiceApplicationMapper;
 import com.lgyun.system.order.service.*;
 import com.lgyun.system.order.vo.ApplicationVO;
@@ -13,6 +15,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.math.BigDecimal;
 import java.util.Date;
 import java.util.List;
 
@@ -29,37 +32,35 @@ public class InvoiceApplicationServiceImpl extends BaseServiceImpl<InvoiceApplic
 
     private IInvoiceApplicationPayListService iInvoiceApplicationPayListService;
     private IUserClient iUserClient;
-    private IPlatformInvoicePayListService platformInvoicePayListService;
-    private IPlatformInvoiceService platformInvoiceService;
 
     @Override
     @Transactional
     public R contractApplyInvoice(ContractApplyInvoiceDTO contractApplyInvoiceDto, Long enterpriseId, IPayEnterpriseService payEnterpriseService) {
-        Long payEnterpriseId = contractApplyInvoiceDto.getPayEnterpriseId();
-        PayEnterpriseEntity byId = payEnterpriseService.getById(payEnterpriseId);
-        if(null == byId){
-            return R.fail("支付清单不存在");
+        String payEnterpriseIds = contractApplyInvoiceDto.getPayEnterpriseIds();
+        String[] split = payEnterpriseIds.split(",");
+        for(int i = 0;i < split.length; i++){
+            List<ApplicationVO> application = iInvoiceApplicationPayListService.findApplication(Long.parseLong(split[i]));
+            if(application.size() > 0){
+                return R.fail("申请记录已存在，请耐心等候！！！");
+            }
         }
-        List<ApplicationVO> application = iInvoiceApplicationPayListService.findApplication(payEnterpriseId);
-        if(application.size() > 0){
-            return R.fail("申请记录已存在，请耐心等候！！！");
-        }
-        PlatformInvoicePayListEntity platformInvoicePayListEntity = platformInvoicePayListService.findPayEnterpriseId(payEnterpriseId);
-        PlatformInvoiceEntity byId1 = platformInvoiceService.getById(platformInvoicePayListEntity.getInvoicePrintId());
 
         InvoiceApplicationEntity invoiceApplicationEntity = new InvoiceApplicationEntity();
         invoiceApplicationEntity.setApplicationDesc(contractApplyInvoiceDto.getApplicationDesc());
-        invoiceApplicationEntity.setVoiceTotalAmount(contractApplyInvoiceDto.getVoiceTotalAmount());
         invoiceApplicationEntity.setApplicationPerson(iUserClient.queryEnterpriseById(enterpriseId).getEnterpriseName());
         invoiceApplicationEntity.setInvoiceCatalogId(contractApplyInvoiceDto.getInvoiceCatalogId());
         invoiceApplicationEntity.setApplicationDate(new Date());
+        BigDecimal voiceTotalAmount = BigDecimal.ZERO;
+        for(int i = 0;i < split.length; i++){
+            PayEnterpriseEntity byId = payEnterpriseService.getById(Long.parseLong(split[i]));
+            voiceTotalAmount = voiceTotalAmount.subtract(byId.getTotalMakerNetIncome());
+            InvoiceApplicationPayListEntity invoiceApplicationPayListEntity = new InvoiceApplicationPayListEntity();
+            invoiceApplicationPayListEntity.setPayEnterpriseId(Long.parseLong(split[i]));
+            invoiceApplicationPayListEntity.setApplicationId(invoiceApplicationEntity.getId());
+            iInvoiceApplicationPayListService.save(invoiceApplicationPayListEntity);
+        }
+        invoiceApplicationEntity.setVoiceTotalAmount(voiceTotalAmount);
         save(invoiceApplicationEntity);
-        byId1.setApplicationId(invoiceApplicationEntity.getId());
-        platformInvoiceService.saveOrUpdate(byId1);
-        InvoiceApplicationPayListEntity invoiceApplicationPayListEntity = new InvoiceApplicationPayListEntity();
-        invoiceApplicationPayListEntity.setPayEnterpriseId(contractApplyInvoiceDto.getPayEnterpriseId());
-        invoiceApplicationPayListEntity.setApplicationId(invoiceApplicationEntity.getId());
-        iInvoiceApplicationPayListService.save(invoiceApplicationPayListEntity);
         return R.success("申请成功");
     }
 }
