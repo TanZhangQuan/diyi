11f842f735d43c591c572151be490c76610cd016
tanzhangquan <1418541909@qq.com>
2020-11-11 10:58:22 +0800

    调整修改

--- diyi-service/diyi-user/src/main/java/com/lgyun/system/user/controller/serviceProvider/NaturalPersonMakerServiceProviderController.java
+++ diyi-service/diyi-user/src/main/java/com/lgyun/system/user/controller/serviceProvider/NaturalPersonMakerServiceProviderController.java
@@ -63,7 +63,7 @@ public class NaturalPersonMakerServiceProviderController {
         }
         ServiceProviderWorkerEntity serviceProviderWorkerEntity = result.getData();
 
-        return enterpriseServiceProviderService.queryRelevanceEnterpriseList(serviceProviderWorkerEntity.getServiceProviderId(), keyword, Condition.getPage(query.setDescs("t1.create_time")));
+        return enterpriseServiceProviderService.queryRelevanceEnterpriseList(serviceProviderWorkerEntity.getServiceProviderId(), keyword, Condition.getPage(query.setDescs("t4.create_time")));
     }
 
     @GetMapping("/query-relevance-maker-list-by-enterprise-id")
