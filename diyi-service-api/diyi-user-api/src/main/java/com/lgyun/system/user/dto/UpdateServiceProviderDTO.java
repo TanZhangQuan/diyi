39db06c961a41d90b695c2d29ae5b309425e26f8
tanzhangquan <1418541909@qq.com>
2020-11-12 11:04:04 +0800

    调整修改

--- diyi-service-api/diyi-user-api/src/main/java/com/lgyun/system/user/dto/UpdateServiceProviderDTO.java
+++ diyi-service-api/diyi-user-api/src/main/java/com/lgyun/system/user/dto/UpdateServiceProviderDTO.java
@@ -40,7 +40,6 @@ public class UpdateServiceProviderDTO implements Serializable {
     /**
      * 法人身份证
      */
-    @NotBlank(message = "请输入法人身份证")
     private String legalPersonIdcard;
 
     /**
