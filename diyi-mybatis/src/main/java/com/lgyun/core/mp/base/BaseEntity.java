1346dc41e0473c2350bdb21bd320630c7cd48a87
tanzhangquan <1418541909@qq.com>
2020-11-28 09:48:25 +0800

    调整修改

--- diyi-mybatis/src/main/java/com/lgyun/core/mp/base/BaseEntity.java
+++ diyi-mybatis/src/main/java/com/lgyun/core/mp/base/BaseEntity.java
@@ -3,11 +3,7 @@ package com.lgyun.core.mp.base;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableLogic;
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import lombok.Data;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import java.io.Serializable;
 import java.util.Date;
@@ -21,39 +17,30 @@ import java.util.Date;
 @Data
 public class BaseEntity implements Serializable {
 
-    public static final String PATTERN_DATETIME = "yyyy-MM-dd HH:mm:ss";
-
     /**
      * 主键
      */
     @TableId(value = "id", type = IdType.ASSIGN_ID)
-    @JsonSerialize(using = ToStringSerializer.class)
     private Long id;
 
     /**
      * 创建人
      */
-    @JsonSerialize(using = ToStringSerializer.class)
     private Long createUser;
 
     /**
      * 创建时间
      */
-    @DateTimeFormat(pattern = PATTERN_DATETIME)
-    @JsonFormat(pattern = PATTERN_DATETIME, timezone = "GMT+8")
     private Date createTime;
 
     /**
      * 更新人
      */
-    @JsonSerialize(using = ToStringSerializer.class)
     private Long updateUser;
 
     /**
      * 更新时间
      */
-    @DateTimeFormat(pattern = PATTERN_DATETIME)
-    @JsonFormat(pattern = PATTERN_DATETIME, timezone = "GMT+8")
     private Date updateTime;
 
     /**
