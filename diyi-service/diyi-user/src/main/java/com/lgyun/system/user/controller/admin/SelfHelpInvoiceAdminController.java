d68ef398dda8c611156e55180091a70de0754d2a
tanzhangquan <1418541909@qq.com>
2020-12-16 10:37:06 +0800

    调整修改

--- diyi-service/diyi-user/src/main/java/com/lgyun/system/user/controller/admin/SelfHelpInvoiceAdminController.java
+++ diyi-service/diyi-user/src/main/java/com/lgyun/system/user/controller/admin/SelfHelpInvoiceAdminController.java
@@ -6,11 +6,9 @@ import com.lgyun.core.mp.support.Condition;
 import com.lgyun.core.mp.support.Query;
 import com.lgyun.system.user.dto.EnterpriseListDTO;
 import com.lgyun.system.user.entity.AdminEntity;
-import com.lgyun.system.user.entity.EnterpriseWorkerEntity;
 import com.lgyun.system.user.service.IAdminService;
 import com.lgyun.system.user.service.IEnterpriseService;
 import com.lgyun.system.user.service.IEnterpriseServiceProviderService;
-import com.lgyun.system.user.service.IEnterpriseWorkerService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
@@ -27,21 +25,22 @@ import javax.validation.constraints.NotNull;
 @RequestMapping("/admin/self-help-invoice")
 @Validated
 @AllArgsConstructor
-@Api(value = "商户端---自助开票管理模块相关接口", tags = "商户端---自助开票管理模块相关接口")
+@Api(value = "平台端---自助开票管理模块相关接口", tags = "平台端---自助开票管理模块相关接口")
 public class SelfHelpInvoiceAdminController {
+
     private IAdminService adminService;
-    private IEnterpriseServiceProviderService enterpriseServiceProviderService;
     private IEnterpriseService enterpriseService;
+    private IEnterpriseServiceProviderService enterpriseServiceProviderService;
 
     @GetMapping("/query-cooperation-service-provider-list")
     @ApiOperation(value = "查询商户合作服务商", notes = "查询商户合作服务商")
-    public R queryCooperationServiceProviderList(@ApiParam(value = "商户id",required = true) @NotNull(message = "请选择商户") @RequestParam(required = false) Long enterpriseId, Query query, BladeUser bladeUser) {
+    public R queryCooperationServiceProviderList(@ApiParam(value = "商户id", required = true) @NotNull(message = "请选择商户") @RequestParam(required = false) Long enterpriseId, Query query, BladeUser bladeUser) {
         //查询当前管理员
         R<AdminEntity> result = adminService.currentAdmin(bladeUser);
         if (!(result.isSuccess())) {
             return result;
         }
-        return enterpriseServiceProviderService.queryCooperationServiceProviderList(enterpriseId,null,Condition.getPage(query.setDescs("t1.create_time")));
+        return enterpriseServiceProviderService.queryCooperationServiceProviderList(enterpriseId, null, Condition.getPage(query.setDescs("t1.create_time")));
     }
 
     @GetMapping("/query-enterprise-list")
@@ -53,7 +52,7 @@ public class SelfHelpInvoiceAdminController {
             return result;
         }
 
-        return enterpriseService.queryEnterpriseListAdmin(enterpriseListDTO, Condition.getPage(query.setDescs("t1.create_time")));
+        return enterpriseService.queryEnterpriseList(null, null, enterpriseListDTO, Condition.getPage(query.setDescs("t1.create_time")));
     }
 
 }
