2efc02ff4a17cb759ac720ead27aa19ab84fbfa7
springboot <1418541909@qq.com>
2021-04-16 13:25:42 +0800

    调整修改

--- diyi-common/src/main/java/com/lgyun/common/tool/PDFUtil.java
+++ diyi-common/src/main/java/com/lgyun/common/tool/PDFUtil.java
@@ -1,11 +1,5 @@
 package com.lgyun.common.tool;
 
-import com.itextpdf.text.Document;
-import com.itextpdf.text.Image;
-import com.itextpdf.text.pdf.PdfContentByte;
-import com.itextpdf.text.pdf.PdfReader;
-import com.itextpdf.text.pdf.PdfStamper;
-
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -28,25 +22,25 @@ public class PDFUtil {
         String UUIDStr = UUID.randomUUID().toString();
 
         File htmlFile = File.createTempFile(UUIDStr, ".pdf");
-        // 读取模板文件
-        PdfReader reader = new PdfReader(input);
-        FileOutputStream fileOutputStream = new FileOutputStream(htmlFile);
-        PdfStamper stamper = new PdfStamper(reader, fileOutputStream);
-        Document document = new Document();
-        // 通过域名查询所在页和坐标，左下角为起点
-        float x = document.getPageSize().getWidth() - xCoordinate;
-        float y = document.getPageSize().getHeight() - yCoordinate;
-        // 读图片
-        Image image = Image.getInstance(signPic);
-        // 查询操作的页面
-        PdfContentByte under = stamper.getOverContent(pageSize);
-        // 根据域的大小缩放图片
-        image.scaleToFit(document.getPageSize().getWidth() - 350, document.getPageSize().getHeight() - 290);
-        // 添加图片
-        image.setAbsolutePosition(x, y);
-        under.addImage(image);
-        stamper.close();
-        reader.close();
+//        // 读取模板文件
+//        PdfReader reader = new PdfReader(input);
+//        FileOutputStream fileOutputStream = new FileOutputStream(htmlFile);
+//        PdfStamper stamper = new PdfStamper(reader, fileOutputStream);
+//        Document document = new Document();
+//        // 通过域名查询所在页和坐标，左下角为起点
+//        float x = document.getPageSize().getWidth() - xCoordinate;
+//        float y = document.getPageSize().getHeight() - yCoordinate;
+//        // 读图片
+//        Image image = Image.getInstance(signPic);
+//        // 查询操作的页面
+//        PdfContentByte under = stamper.getOverContent(pageSize);
+//        // 根据域的大小缩放图片
+//        image.scaleToFit(document.getPageSize().getWidth() - 350, document.getPageSize().getHeight() - 290);
+//        // 添加图片
+//        image.setAbsolutePosition(x, y);
+//        under.addImage(image);
+//        stamper.close();
+//        reader.close();
         FileInputStream fileInputStream = new FileInputStream(htmlFile);
         Map map = new HashMap();
         map.put("htmlFile", htmlFile);
